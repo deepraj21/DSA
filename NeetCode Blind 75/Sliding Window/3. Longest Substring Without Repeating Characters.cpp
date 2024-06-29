44b9d8fd81898885fd50b9b3157edaeb1474e769
Deepraj <deepraj21.bera@gmail.com>
2024-06-29 21:01:18 +0530

    Runtime:7 ms Memory:7.9 MB

---
+++ NeetCode Blind 75/Sliding Window/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,50 @@
+// Given a string s, find the length of the longest 
+// substring
+//  without repeating characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int i = 0;
+        int j = 0;
+        unordered_set<char> ele;
+        int res = 0;
+        while (i < s.size())
+        {
+            if (ele.find(s[i]) == ele.end())
+            {
+                ele.insert(s[i]);
+                res = max(res, i - j + 1);
+                i++;
+            }
+            else
+            {
+
+                ele.erase(s[j]);
+                j++;
+            }
+        }
+        return res;
+    }
+};
